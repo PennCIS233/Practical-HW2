2935796e1a37f8c9cbc50904321a6cadd14cd99a
hclord <58673947+hclord@users.noreply.github.com>
2022-02-16 17:53:44 -0500

    Add files via upload

--- frontend/src/pages/ConnectPage.js
+++ frontend/src/pages/ConnectPage.js
@@ -5,24 +5,32 @@ import NavBar from "../components/NavBar";
 import { useNavigate } from "react-router-dom";
 
 function ConnectPage() {
-  const [accounts, setAccounts] = useState([]);
-  const [isConnected, setIsConnected] = useState(false);
-
+  /*
+   * Navigate lets us pass stateto the ElectionPage component.
+   */
   let navigate = useNavigate();
 
-  // connectAlgoSigner
-  // Description:
-  // retrieves the user accounts from AlgoSigner
+  /*
+   * Here we define the stored state for this component.
+   */
+  const [accounts, setAccounts] = useState([]); // list of user addresses connected to AlgoSigner
+  const [isConnected, setIsConnected] = useState(false); // boolean that is true if the page is connected to AlgoSigner
+
+  /* connectAlgoSigner
+   * Description:
+   * retrieves the user accounts from AlgoSigner
+   */
   const connectAlgoSigner = async () => {
     let newAccounts = await mainAlgoHandler.getAlgoSignerAccounts();
     setAccounts(newAccounts);
     setIsConnected(true);
   };
 
-  // handleElectionSubmit
-  // Description:
-  // takes the value inputted into the appID form and
-  // naviagates to the election page
+  /* handleElectionSubmit
+   * Description:
+   *  takes the value inputted into the appID form and naviagates to ElectionPage,
+   *  sending along the relevant state (accounts and appID)
+   * */
   const handleElectionSubmit = async (e) => {
     e.preventDefault();
     const formData = new FormData(e.target);
@@ -33,27 +41,25 @@ function ConnectPage() {
     });
   };
 
+  /*
+   * Render the connect page. It displays the NavBar at the top of the page,
+   * as well as a button to connect to AlgoSigner and a form to fill in the app ID.
+   */
   return (
     <>
       <NavBar />
       <Container>
         <Row className="px-3 mt-3">
-          <Row>
-            <Col>
-              <Card className="mt-5">
-                <Card.Body>
-                  <Card.Title>Connect to AlgoSigner</Card.Title>
-                  <Button
-                    variant="info"
-                    onClick={connectAlgoSigner}
-                    disabled={isConnected}
-                  >
-                    Connect
-                  </Button>
-                </Card.Body>
-              </Card>
-            </Col>
-          </Row>
+          <Col>
+            <Card className="mt-5">
+              <Card.Body>
+                <Card.Title>Connect to AlgoSigner</Card.Title>
+                <Button variant="info" onClick={connectAlgoSigner}>
+                  Connect
+                </Button>
+              </Card.Body>
+            </Card>
+          </Col>
         </Row>
       </Container>
       <Container>
@@ -61,14 +67,14 @@ function ConnectPage() {
           <Col>
             <Card>
               <Card.Body>
-                <Card.Title>Election ID</Card.Title>
+                <Card.Title>App ID</Card.Title>
                 <Form onSubmit={async (e) => await handleElectionSubmit(e)}>
                   <Form.Group className="mb-3" controlId="appID">
                     <Form.Control
                       disabled={!isConnected}
                       type="election"
                       name="appID"
-                      placeholder="Enter election id"
+                      placeholder="Enter app id"
                     />
                   </Form.Group>
                   <Button variant="info" type="submit">
--- frontend/src/pages/ElectionPage.js
+++ frontend/src/pages/ElectionPage.js
@@ -1,8 +1,7 @@
 import React, { useEffect, useState } from "react";
-import { Row, Col, Container, CardGroup } from "react-bootstrap";
+import { Row, Col, Container, Modal, Button } from "react-bootstrap";
 import mainAlgoHandler from "../components/AlgoHandler";
 import NavBar from "../components/NavBar";
-
 import VoterCard from "../components/VoterCard";
 import ParticipantsCard from "../components/ParticipantsCard";
 import ElectionInfoCard from "../components/ElectionInfoCard";
@@ -10,49 +9,112 @@ import ElectionInfoCard from "../components/ElectionInfoCard";
 import { useLocation } from "react-router-dom";
 
 function ElectionPage() {
+  /*
+   * Location lets us access the state (appID and accounts) passed to the component from the ConnectPage.
+   */
   let location = useLocation();
+  const appID = location.state.appID; // appID that the user entered
+  const accounts = location.state.accts; // accounts that are connected to AlgoSigner
 
-  // constant variables for page
-  const appID = location.state.appID;
-  const accounts = location.state.accts;
-
-  // variables that change state
-  const [electionState, setElectionState] = useState({});
-  const [mainAccount, setMainAccount] = useState(accounts[0]);
-  const [totalVotes, setTotalVotes] = useState([]);
-  const [electionChoices, setElectionChoices] = useState([]);
-  const [userVotes, setUserVotes] = useState({});
+  /*
+   * Here we define the stored state for this component.
+   */
+  const [isError, setIsError] = useState(false); // boolean set to true if there is an error retrieving the electionState
+  const [electionState, setElectionState] = useState({}); // JSON containing all global variables for the application
+  const [mainAccount, setMainAccount] = useState(
+    accounts.length > 0 ? accounts[0] : ""
+  ); // string that is set to the current account
+  const [totalVotes, setTotalVotes] = useState([]); // array of integers storing the total number of votes for each choice
+  const [electionChoices, setElectionChoices] = useState([]); // array of strings listing the choices in the election
+  const [userVotes, setUserVotes] = useState({}); // JSON containing a mapping of user addresses to their votes in the election
   const [optedAccounts, setOptedAccounts] = useState({
     maybe: [],
     yes: [],
     no: [],
-  });
+  }); // JSON of lists containing the user addresses who have are accepted, rejected, and pending
 
-  // refreshState
-  // Description:
-  //  Calls API to get election state and list of all users that have opted-in
+  /* refreshState
+   * Description:
+   * Calls API to get election state and list of all users that have opted-in.
+   */
   const refreshState = () => {
     console.log("refreshing state...");
-    // TODO: connect to AlgoHandler
+
+    mainAlgoHandler
+      .getElectionState(location.state.appID)
+      .then((res) => {
+        let newTotalVotes = [];
+        for (let i = 0; i < res["NumVoteOptions"]; i++) {
+          newTotalVotes.push(res[`VotesFor${i}`]);
+        }
+
+        let newElectionChoices = res["VoteOptions"].split(",");
+        setElectionState(res);
+        setTotalVotes(newTotalVotes);
+        setElectionChoices(newElectionChoices);
+      })
+      .catch((err) => {
+        console.log(err);
+        setIsError(true);
+      });
+
+    console.log(electionState);
+
+    mainAlgoHandler
+      .getAllLocalStates(parseInt(appID))
+      .then((allLocalStates) => {
+        let newOptedAccounts = {
+          yes: [],
+          no: [],
+          maybe: [],
+        };
+        for (const address in allLocalStates) {
+          let canVote = allLocalStates[address]["can_vote"];
+          if ("can_vote" in allLocalStates[address]) {
+            newOptedAccounts[canVote].push(address);
+          }
+        }
+        setOptedAccounts(newOptedAccounts);
+
+        let newUserVotes = {};
+        for (const address in allLocalStates) {
+          if ("voted" in allLocalStates[address]) {
+            newUserVotes[address] = allLocalStates[address]["voted"];
+          }
+        }
+        setUserVotes(newUserVotes);
+      })
+      .catch((err) => {
+        console.log(err);
+        setIsError(true);
+      });
   };
 
-  // useEffect
-  // Description:
-  //  Retrieves election state when component is first rendered
+  /* useEffect
+   * Description:
+   *  Retrieves election state when component is first rendered
+   */
   useEffect(() => {
     refreshState();
   }, []);
 
-  // handleMainAccountChange
-  // Description:
-  //  Updates the main account and updates the state
-  // Parameters:
-  //   user (string) - user to change main account to
+  /* handleMainAccountChange
+   * Description:
+   *  Updates the main account and updates the state
+   * Parameters:
+   *  user (string) - user to change main account to
+   */
   const handleMainAccountChange = (user) => {
     setMainAccount(user);
     refreshState();
   };
 
+  /*
+   * Render the main election page. It displays the NavBar at the top of the page,
+   * as well as three Cards - the global election info, the election participants,
+   * and a form to participate in the election. If isError is true, then a Modal
+   * (a popup) is displayed.
+   */
   return (
     <>
       <NavBar
@@ -63,8 +125,8 @@ function ElectionPage() {
         refreshState={refreshState}
       />
       <Container>
-        <Row className="mt-3 align-items-center">
-          <CardGroup>
+        <Row xs={1} md={2} className="g-4 mt-3">
+          <Col>
             <ParticipantsCard
               appID={appID}
               users={accounts}
@@ -74,28 +136,43 @@ function ElectionPage() {
               optedAccounts={optedAccounts}
               electionChoices={electionChoices}
             />
+            {accounts.length > 0 && (
+              <VoterCard
+                user={mainAccount}
+                appID={appID}
+                electionState={electionState}
+                isAccepted={optedAccounts["yes"].includes(mainAccount)}
+                isPending={optedAccounts["maybe"].includes(mainAccount)}
+                isRejected={optedAccounts["no"].includes(mainAccount)}
+                isVoted={userVotes[mainAccount]}
+                electionChoices={electionChoices}
+              />
+            )}
+          </Col>
+
+          <Col>
             <ElectionInfoCard
               currVotes={totalVotes}
               appID={appID}
               state={electionState}
             />
-          </CardGroup>
-        </Row>
-        <Row>
-          <Col>
-            <VoterCard
-              user={mainAccount}
-              appID={appID}
-              electionState={electionState}
-              isAccepted={optedAccounts["yes"].includes(mainAccount)}
-              isPending={optedAccounts["maybe"].includes(mainAccount)}
-              isRejected={optedAccounts["no"].includes(mainAccount)}
-              isVoted={userVotes[mainAccount]}
-              electionChoices={electionChoices}
-            />
           </Col>
         </Row>
       </Container>
+      <Modal show={isError}>
+        <Modal.Header>
+          <Modal.Title>Error</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          There was an error when retrieving the application state. Please check
+          that you entered the correct application ID.
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="info" href="/">
+            Return to Connect Page
+          </Button>
+        </Modal.Footer>
+      </Modal>
     </>
   );
 }
