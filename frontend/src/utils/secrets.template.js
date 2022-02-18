d4486be7aa9aeba260e2857185422c5404f65767
Elijah Jasso <57808638+elijahj19@users.noreply.github.com>
2022-02-18 05:40:16 -0500

    export secrets

--- frontend/src/utils/secrets.template.js
+++ frontend/src/utils/secrets.template.js
@@ -1,9 +1,7 @@
-const secrets = {
+export const secrets = {
   algodHeader: {
     "X-API-Key": "Your Purestake API key",
   },
   algodServer: "https://testnet-algorand.api.purestake.io/ps2",
   indexerServer: "https://testnet-algorand.api.purestake.io/idx2"
 }
-
-export default secrets;
