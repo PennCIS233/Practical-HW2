e5b4d6590ace693cbcc03776466c87d43670a241
Angela Lin <30933507+angelalin01@users.noreply.github.com>
2022-02-20 15:40:17 -0500

    Update election_smart_contract.py

--- smart-contract/election_smart_contract.py
+++ smart-contract/election_smart_contract.py
@@ -5,17 +5,17 @@ from pyteal_helper import itoa
 
 def approval_program():
     
-    '''APPROVAL PROGRAM handles the main logic of the application'''
+    """APPROVAL PROGRAM handles the main logic of the application"""
     
     i = ScratchVar(TealType.uint64) # i-variable for for-loop
     
     
     on_creation = Seq(
         [   
-            '''TODO:
-            Check number of required arguments are present
-            Store relevant parameters of the election. When storing the options to vote for, consider storing all of them as a string separated by commas e.g: "A,B,C,D". Note that index-wise, A=0, B=1, C=2, D=3
-            Set all initial vote tallies to 0 for all vote options, keys are the vote options '''
+            #TODO:
+            #Check number of required arguments are present
+            #Store relevant parameters of the election. When storing the options to vote for, consider storing all of them as a string separated by commas e.g: "A,B,C,D". Note that index-wise, A=0, B=1, C=2, D=3
+            #Set all initial vote tallies to 0 for all vote options, keys are the vote options
             
             For(
                 
@@ -28,21 +28,21 @@ def approval_program():
         ]
     )
 
-    ''' call to determine whether the current transaction sender is the creator '''
+   # call to determine whether the current transaction sender is the creator
     is_creator = Txn.sender() == App.globalGet(Bytes("Creator"))
     
-    ''' value of whether or not the sender can vote ("yes", "no", or "maybe") '''
+    # value of whether or not the sender can vote ("yes", "no", or "maybe")
     get_sender_can_vote = App.localGetEx(Int(0), App.id(), Bytes("can_vote"))
 
-    ''' get_vote_sender is a value that the sender voted for, a number indicating index in the VoteOptions string faux-array.
-        Remember that since we stored the election's voting options as a string separated by commas (such as "A,B,C,D"),
-        If a user wants to vote for C, then the choice that the user wants to vote for is equivalent to "3". '''
+    # get_vote_sender is a value that the sender voted for, a number indicating index in the VoteOptions string faux-array.
+    # Remember that since we stored the election's voting options as a string separated by commas (such as "A,B,C,D"),
+    # If a user wants to vote for C, then the choice that the user wants to vote for is equivalent to "3".
     get_vote_of_sender = App.localGetEx(Int(0), App.id(), Bytes("voted"))
     
 
    
     on_closeout = Seq(
-        ''' TODO: CLOSE OUT: ''' 
+        # TODO: CLOSE OUT:
         [
             
 
@@ -52,20 +52,20 @@ def approval_program():
 
     
     on_register = Seq(
-        ''' TODO: REGISTRATION: '''
+        # TODO: REGISTRATION:
         Return(Int(1))
     )
 
     
     on_update_user_status = Seq(
-        ''' TODO: UPDATE USER LOGIC '''
+        # TODO: UPDATE USER LOGIC
         Return(Int(1))
     )
 
    
     choice = Btoi(Txn.application_args[1])
     on_vote = Seq(
-         ''' TODO: USER VOTING LOGIC: '''
+         # TODO: USER VOTING LOGIC: 
         [
             
 
@@ -76,7 +76,7 @@ def approval_program():
     
     program = Cond(
         
-        ''' MAIN CONDITIONAL '''
+        # MAIN CONDITIONAL
         
         [Txn.application_id() == Int(0), on_creation],
         [Txn.on_completion() == OnComplete.DeleteApplication, Return(is_creator)],
@@ -84,7 +84,7 @@ def approval_program():
         [Txn.on_completion() == OnComplete.CloseOut, on_closeout],
         [Txn.on_completion() == OnComplete.OptIn, on_register],
         
-        ''' TODO: Complete the cases that will trigger the update_user_status and on_vote sequences '''
+        # TODO: Complete the cases that will trigger the update_user_status and on_vote sequences
         
     )
 
@@ -100,7 +100,7 @@ def clear_state_program():
 
     
     program = Seq(
-        ''' remove their vote from the correct vote tally '''
+        # remove their vote from the correct vote tally
         [
         
 
